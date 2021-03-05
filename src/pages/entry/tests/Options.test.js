dd5718f149978afc6171acd24e5f188acc1d9281
William Barbosa <wbarbosa@fullstacklabs.co>
2021-03-05 14:21:18 -0300

    Code and test scoop opts images from a server

---
+++ src/pages/entry/tests/Options.test.js
@@ -0,0 +1,14 @@
+import { render, screen } from "@testing-library/react";
+
+import Options from "../Options";
+
+it("Should display image from each scoop opt from server", async () => {
+  render(<Options optionType="scoops" />);
+
+  const scoopImages = await screen.findAllByRole("img", { name: /scoop$/i });
+  expect(scoopImages).toHaveLength(2);
+
+  //confirm alt text of images
+  const altText = scoopImages.map((element) => element.alt);
+  expect(altText).toEqual(["Chocolate scoop", "Vanilla scoop"]);
+});
